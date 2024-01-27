ac6611e665a1b0bb284e367cbf76b47369a3443f
Nirav Soni <soninirav910@gmail.com>
2024-01-27 17:03:43 +0530

    Revert service locattor pattern changes

--- ProductImporter.Logic/ProductImporter.cs
+++ ProductImporter.Logic/ProductImporter.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using ProductImporter.Logic.Shared;
+﻿using ProductImporter.Logic.Shared;
 using ProductImporter.Logic.Source;
 using ProductImporter.Logic.Target;
 using ProductImporter.Logic.Transformation;
@@ -9,22 +8,18 @@ namespace ProductImporter.Logic;
 public class ProductImporter
 {
     private readonly IProductSource _productSource;
-    private readonly IServiceProvider _serviceProvider;
-    //private readonly IProductTransformer _productTransformer;
+    private readonly IProductTransformer _productTransformer;
     private readonly IProductTarget _productTarget;
     private readonly IImportStatistics _importStatistics;
 
     public ProductImporter(
         IProductSource productSource, 
-        IServiceProvider serviceProvider,
-        //Remove IProductTransformer and inject IServiceProvider
-        //IProductTransformer productTransformer,
+        IProductTransformer productTransformer,
         IProductTarget productTarget, 
         IImportStatistics importStatistics)
     {
         _productSource = productSource;
-        _serviceProvider = serviceProvider;
-        //_productTransformer = productTransformer;
+        _productTransformer = productTransformer;
         _productTarget = productTarget;
         _importStatistics = importStatistics;
     }
@@ -38,16 +33,7 @@ public class ProductImporter
         {
             var product = _productSource.GetNextProduct();
 
-            //Throws InvalidOperationException when implementation is missing
-            //var serviceProvider = _serviceProvider.GetRequiredService<IProductTransformer>();
-
-            //Does not throw exception but returns null when type registration is missing
-            var productTransformer = _serviceProvider.GetService<IProductTransformer>();
-
-            
-            var transformedProduct = productTransformer == null 
-                ? product
-                : productTransformer.ApplyTransformations(product);
+            var transformedProduct = _productTransformer.ApplyTransformations(product);
 
             _productTarget.AddProduct(transformedProduct);
         }
