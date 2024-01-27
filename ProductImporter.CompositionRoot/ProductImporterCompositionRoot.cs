334171987ca0056ea9aa9b120585905f74660963
Nirav Soni <soninirav910@gmail.com>
2024-01-27 15:03:43 +0530

    Updates

---
+++ ProductImporter.CompositionRoot/ProductImporterCompositionRoot.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductImporter.Logic.Shared;
+using ProductImporter.Logic.Source;
+using ProductImporter.Logic.Target;
+using ProductImporter.Logic.Transformation.Util;
+using ProductImporter.Logic.Transformation;
+using ProductImporter.Logic.Transformations;
+using ProductImporter.Transformations;
+
+namespace ProductImporter.CompositionRoot
+{
+    public static class ProductImporterCompositionRoot
+    {
+        public static IServiceCollection AddProductImporter(this IServiceCollection services)
+        {
+            services.AddSingleton<Configuration>();
+
+            services.AddTransient<IPriceParser, PriceParser>();
+            services.AddTransient<IProductSource, ProductSource>();
+
+            services.AddTransient<IProductFormatter, ProductFormatter>();
+            services.AddTransient<IProductTarget, CsvProductTarget>();
+
+            services.AddTransient<Logic.ProductImporter>();
+
+            services.AddSingleton<IImportStatistics, ImportStatistics>();
+
+            services.AddTransient<IProductTransformer, ProductTransformer>();
+
+            services.AddScoped<IProductTransformationContext, ProductTransformationContext>();
+            services.AddScoped<INameDecapitaliser, NameDecapitaliser>();
+            services.AddScoped<ICurrencyNormalizer, CurrencyNormalizer>();
+
+            services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+            services.AddScoped<IReferenceAdder, ReferenceAdder>();
+            services.AddScoped<IReferenceGenerator, ReferenceGenerator>();
+            services.AddSingleton<IIncrementingCounter, IncrementingCounter>();
+            return services;
+        }
+    }
+}
--- ProductImporter/Program.cs
+++ ProductImporter/Program.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ProductImporter.Logic.Shared;
-using ProductImporter.Logic.Source;
-using ProductImporter.Logic.Target;
-using ProductImporter.Logic.Transformation;
-using ProductImporter.Logic.Transformations;
-using ProductImporter.Logic.Transformation.Util;
-
-using ProductImporter.Transformations;
+using ProductImporter.CompositionRoot;
 
 using var host = Host.CreateDefaultBuilder(args)
     .UseDefaultServiceProvider((context, options) => {
@@ -15,28 +8,7 @@ using var host = Host.CreateDefaultBuilder(args)
     })
     .ConfigureServices((context, services) =>
     {
-        services.AddSingleton<Configuration>();
-
-        services.AddTransient<IPriceParser, PriceParser>();
-        services.AddTransient<IProductSource, ProductSource>();
-
-        services.AddTransient<IProductFormatter, ProductFormatter>();
-        services.AddTransient<IProductTarget, CsvProductTarget>();
-
-        services.AddTransient<ProductImporter.Logic.ProductImporter>();
-
-        services.AddSingleton<IImportStatistics, ImportStatistics>();
-
-        services.AddTransient<IProductTransformer, ProductTransformer>();
-
-        services.AddScoped<IProductTransformationContext, ProductTransformationContext>();
-        services.AddScoped<INameDecapitaliser, NameDecapitaliser>();
-        services.AddScoped<ICurrencyNormalizer, CurrencyNormalizer>();
-
-        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
-        services.AddScoped<IReferenceAdder, ReferenceAdder>();
-        services.AddScoped<IReferenceGenerator, ReferenceGenerator>();
-        services.AddSingleton<IIncrementingCounter, IncrementingCounter>();
+        services.AddProductImporter();
     })
     .Build();
 
